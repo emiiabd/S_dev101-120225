bf61831bf3087d723fc9b7cc69d0bb5922f30dc0
emiiabd <abdalaemiliano@gmail.com>
2025-02-17 20:21:20 -0300

    Build

--- src/server.js
+++ src/server.js
@@ -9,10 +9,23 @@ import frontENVrouter from './routes/frontENV.router.js';
 
 const app = express();
 const port = 3000;
+//Body Parser
+app.use(express.json({ extended: false, limit: '50mb'}));
+app.use(express.urlencoded({extended: false, limit: '50mb' }));
+
+//Como hostear react directo desde express? Asi --> 
+//Primero le decimos a express que use todos los archivos del build de react asi:
+app.use(express.static(__dirname + "/Seguridad101-Front/build"));
+//Luego le decimos a express que sirva todo eso desde el home
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/Seguridad101-Front/build", "index.html"))
+});
 
 app.use(cors());
 app.use(express.json({limit: '15mb'}));
 app.use(verifyApiKeyMiddleware)
+
+
 
 app.use('/api/auth', authRouter);
 app.use('/api/services', servicesRouter);
